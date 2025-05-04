d48e660fc47640dafaa6ee1ac78a96cdbd10f9e3
Ayush Verma <ayushverma2112004@gmail.com>
2025-05-05 04:04:26 +0530

    done with the project

---
+++ src/components/Body.js
@@ -0,0 +1,94 @@
+import RestaurantCard, {withPromotedLabel} from "./RestaurantCard";
+import { useEffect, useState , useContext} from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utilis/useOnlineStatus";
+import userContext from "../utilis/UserContext";
+
+// definition of state react variable
+// ! Whenever state variable updates, react triggers a reconciliation cycle (re-renders the component)
+
+const Body = () => {
+  const [listOfRestaurant, setlistOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
+
+  console.log("body rendering", listOfRestaurant);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=28.63270&lng=77.21980&carousel=true&third_party_vendor=1"
+    );
+    const json = await data.json();
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setlistOfRestaurant(
+      // optional chaining:-
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const onlineStatus = useOnlineStatus();
+  if (onlineStatus === false) {
+    return (<h1>Looks like you are offline !! check your Internet Connection</h1>)
+  }
+
+  const {isLoggedUser ,setLoggedinInfo} = useContext(userContext)
+
+  // conditional rendering - Rendering on the basis of Condition
+  // if (listOfRestaurant.length === 0) {
+  //   return <Shimmer/>
+  // }
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">    
+      <div className="cont-bn">
+      <div className="search-box">
+      <input type="text" value={searchText} placeholder="Search" className="search" onChange={(e)=>{setSearchText(e.target.value)}} ></input>
+      <button className="search-btn" onClick={() => {
+          const filteredRestaurant = listOfRestaurant.filter((res)=>{
+              return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+          })
+          
+              setFilteredRestaurant(filteredRestaurant);
+      }}>Search</button>
+
+      
+
+  </div>
+        <button className="filter-btn" onClick={() => {
+          const filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4);
+          setFilteredRestaurant(filteredList);
+      }}>Top Rated Restaurants</button>
+        
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurant/" + restaurant.info.id}
+          >
+            {restaurant.info.isOpen ? (
+              <RestaurantCardPromoted resData={restaurant} />
+            ) : (
+              <RestaurantCard resData={restaurant} />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/utilis/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+export const LOGO_URL =
+  "https://imgs.search.brave.com/1xyB2TpoXLfrTBMOOgITGzGzU63Nd6ZyRwrM6JnWIVM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9kZXNp/Z25pbWFnZXMuYXBw/eXBpZS5jb20vZmF2/aWNvbi9mb29kZmF2/aWNvbi02LXBsYW50/LWZvb2QuanBn";
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6351015&lng=77.43922049999999&restaurantId=";

f6634e663f1d710400426095930454574ca7a427
Ayush Verma <114644126+ayuushverma@users.noreply.github.com>
2025-09-15 11:48:43 +0530

    Update Grocery.js

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -2,7 +2,6 @@ const Grocery = () => {
     return (
         <h1>
             {""}
-            Our Grocery Store ,and it have a lot of child component inside the web page.
         </h1>
     )
 }
